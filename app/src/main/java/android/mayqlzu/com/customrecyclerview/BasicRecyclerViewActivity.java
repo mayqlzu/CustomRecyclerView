1eeaa467dea1245fec96ed394abbca091a6bdac4
pony <mayongqiang@ppdai.com>
2018-05-25 13:31:17 +0800

    setOnClickListener() for item and item's child.

--- app/src/main/java/android/mayqlzu/com/customrecyclerview/BasicRecyclerViewActivity.java
+++ app/src/main/java/android/mayqlzu/com/customrecyclerview/BasicRecyclerViewActivity.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,10 +46,12 @@ public class BasicRecyclerViewActivity extends Activity {
 
         public static class ViewHolder extends RecyclerView.ViewHolder {
             public TextView tv;
+            public Button btn;
 
-            public ViewHolder(TextView tv) {
-                super(tv);
-                this.tv = tv;
+            public ViewHolder(View v) {
+                super(v);
+                this.tv = v.findViewById(R.id.tv);
+                this.btn = v.findViewById(R.id.btn);
             }
         }
 
@@ -56,16 +61,52 @@ public class BasicRecyclerViewActivity extends Activity {
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            TextView tv = (TextView) LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.layout_item_text, parent, false);
+            //Log.d("mayq", "onCreateViewHolder(), viewType=" + viewType);
 
-            ViewHolder vh = new ViewHolder(tv);
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_item, parent, false);
+
+            ViewHolder vh = new ViewHolder(v);
+
             return vh;
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            //Log.d("mayq", "onBindViewHolder(), position=" + position);
+
             holder.tv.setText(dataset[position]);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(holder.itemView.getContext(),
+                            "clicked on item " + dataset[position],
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+            holder.tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(holder.itemView.getContext(),
+                            "clicked on tv " + dataset[position],
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+            holder.btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(holder.itemView.getContext(),
+                            "clicked on btn " + dataset[position],
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
         }
 
         @Override
